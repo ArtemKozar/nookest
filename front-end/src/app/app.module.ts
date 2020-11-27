10265109cde6b02335ec9b735e3966d28f93ce26
ArtemKozar <kozarart@gmail.com>
2020-11-27 16:07:44 +0200

    added structure for frot-end

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { EventPageComponent } from './event-page/event-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    EventPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
